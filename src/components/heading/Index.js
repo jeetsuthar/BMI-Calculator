c79bc24a4ab267403b36b21532adb040ace360f7
jeetsuthar <jeetsuthar151@gmail.com>
2024-04-29 22:08:29 +0530

    uploaded BMI calculator

---
+++ src/components/heading/Index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const Index = () => {
+  return (
+    <div className='heading'>
+      <h2 className='fullHeading'>Calculate Your Body Mass Index (BMI) Easily!</h2>
+      <h2 className='shortHeading'>BMI Calculator</h2>
+    </div>
+  )
+}
+
+export default Index
